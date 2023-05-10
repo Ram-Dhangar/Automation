5bb39339737fbd6b09360adc2158cf0d4d12441e
Ramkrushna Dhangar <97275278+Ram-Dhangar@users.noreply.github.com>
2023-05-10 16:44:32 +0530

    Add files via upload
    Bulk migration happened in a particular account where I need to move some files from the source path to the destination and also need to update the same record that the file has been successfully archived in a particular location.

---
+++ move_Migrate_files.ps1
@@ -0,0 +1,78 @@
+﻿function CheckFileModified 
+{
+$data = Import-Csv -Path "D:\temp\220219-11 - Import from file share to Documents.csv"
+#Filter the rows based on a status condition
+$filteredData = $data | Where-Object {$_.Status -eq 'Success' -or $_.Status -eq 'Skipped' -or $_.Status -eq 'Warning' -and $_.Type -eq 'File' }
+
+foreach($files in $filteredData)
+{
+   $FilePath=$files.'Source path'
+   $destinationPath= $files.'Destination path'
+   $Migrateddate = $files.Date
+   Write-Host $FilePath 
+  
+   $Path ="D:\temp\220219-11 - Import from file share to Documents.csv"
+   #$FilePath="D:\source\MasterFile.txt"
+
+
+# Check if the file exists
+
+    if (-not (Test-Path $FilePath)) {
+
+        Write-Error "File not found: $FilePath"
+
+        return
+       }
+
+    # Get the file's last modified date/time
+
+    $LastModifiedDate = (Get-Item $FilePath).LastWriteTime
+    $fileName= (Get-Item $FilePath).Name
+    $LastModifiedDateTime= $LastModifiedDate.ToString("M/d/yyyy")   
+    
+    
+     
+    # Move the file if it hasn't been modified 
+   if ($LastModifiedDateTime -eq $Migrateddate) 
+   {
+     Write-Host "$fileName has not been modified "
+
+     Move-Item $FilePath $destinationPath
+     Write-Host "$fileName has successfully moved to $destinationPath"   
+     $files.'Archive status'="File Archive Successfully"
+     $files.'Archive path'=$destinationPath
+     $files.'Archive FileName'=$fileName
+     
+
+    }  
+   else{
+    Write-Host "$fileName has been modified" 
+      $files.'Archive status' = "File Not Archive" 
+      $files.'Archive FileName' = $fileName
+    }
+
+    $filteredData | Export-Csv $Path -NoTypeInformation
+}
+
+
+}
+
+
+#calling function
+CheckFileModified 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
