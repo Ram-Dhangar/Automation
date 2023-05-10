9097d2953d04e348ddbf43a055a164d8a3985b72
Ramkrushna Dhangar <97275278+Ram-Dhangar@users.noreply.github.com>
2023-05-10 16:27:14 +0530

    Add files via upload
    AD Automation we have a script which will create bulk users in AD using this PowerShell script.

---
+++ Adding user in Bulk.ps1
@@ -0,0 +1,29 @@
+﻿# adding user in bulk in CIS BU
+
+New-ADOrganizationalUnit "CIS" -Description "CIS ACADEMY BATCH 21"
+
+#importing CSV file -name --- CIS_Batch21.csv
+
+$ADuser = Import-Csv C:\CIS\CIS_Batch21.csv
+
+foreach($User in $ADuser)
+{
+  $Username= $User.uname
+  $Password= $User.pass
+  $Firstname= $User.fname
+  $Lastname= $User.lname
+  $Department= $User.dname
+  $OU= $User.ou
+
+  if(Get-ADUser -F{SamAccountName -eq $Username})
+   {
+    Write-Warning "This User account is already exite in directory"
+   }
+   else
+   {
+      New-ADUser -SamAccountName "$Username" -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -DisplayName $Firstname -Path $OU -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Description "Local User" -Department $Department 
+      
+   
+   }
+
+}
