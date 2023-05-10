efd520179a1f80006fb08df4c227ed9d837c1cd7
Ramkrushna Dhangar <97275278+Ram-Dhangar@users.noreply.github.com>
2023-05-10 16:24:08 +0530

    Add files via upload
    the script obtains O365 user creation with specific licenses chosen by the user at the time of the request. this is the next step toward O365 Automation

---
+++ O365 user creation.ps1
@@ -0,0 +1,40 @@
+﻿function Adduser($firstname,$lastname)
+{
+#Install-Module msOnline
+$license=(Get-MsolAccountSku)
+$licenseType=$licencetype[0].AccountSkuId
+
+$upn="$firstname$lastname@LTIMIndtree243.onmicrosoft.com"
+$display="$firstname $lastname"
+$defaultpasswd="Password@123"
+ 
+#Admin user and Password
+$admin_username = "UnnatiJoshi@LTIMIndtree243.onmicrosoft.com"
+$admin_passwd = "Newuser@123"
+$admin_secpasswd = ConvertTo-SecureString -String $admin_passwd -AsPlainText -Force
+$admin_cred = New-Object Management.Automation.PSCredential ($admin_username, $admin_secpasswd)
+Connect-MsolService -Credential $cred
+
+ 
+
+
+#getting the list of users
+
+$user=Get-MsolUser -UserPrincipalName $upn -ErrorAction SilentlyContinue
+if($user -ne $null)
+{
+  Write-Host $upn," User is already exist"
+  
+} 
+else
+{
+ New-MsolUser -DisplayName $display -FirstName $firstname -LastName $lastname -UserPrincipalName $upn -UsageLocation IN -Password $defaultpasswd -ForceChangePassword $true
+ Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $licenseType
+}
+
+
+ 
+}
+
+
+Adduser -firstname "Chetan" -lastname "Baviskar"
